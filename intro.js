d3925be702c280fc9d1e1f169977d6a4789f84f4
oaikhienancornelius97 <corneliusedos@gmail.com>
2022-06-20 22:52:09 +0100

    first commit

--- intro.js
+++ intro.js
@@ -2,4 +2,6 @@ let Name = "Cornelius";
 let Height = 1.67;
 let Country = "Nigeria"
 
-document.getElementById("demo").innerHTML = "My name is" + " " + Name + ", " + "\nI'm " + Height +" "+ "tall" + " " + " and I'm from " + Country
+document.getElementById("demo").innerHTML = "Name:" + " " + Name + ".<br>" + 
+                                            "Height:" + " " + Height +".<br>" +
+                                            "Country:" + " " + Country
